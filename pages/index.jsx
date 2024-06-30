e9ccd161a92765dad8c0cc0c30039a7fc446b0a0
vivektemptest <vivektemptest@gmail.com>
2024-06-30 21:30:31 +0530

    revert changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -84,7 +84,7 @@ const Index = () => {
                   </a>
                   <a href="#skills-section" className="btn-lnk">
                     {" "}
-                    My Skill{" "}
+                    My Skills{" "}
                   </a>
                 </div>
               </div>
